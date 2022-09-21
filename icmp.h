cd04d23c0478d845b399d560800638997b9c4b82
~ch <cigarbox.nami@gmail.com>
2022-09-22 02:50:09 +0900

    step11

--- icmp.h
+++ icmp.h
@@ -1,9 +1,10 @@
-//
-// Created by nami on 9/3/2022.
-//
-
 #ifndef MICROPS_ICMP_H
 #define MICROPS_ICMP_H
+
+#include  <stddef.h>
+#include <stdint.h>
+
+#include "ip.h"
 
 #define ICMP_HDR_SIZE 8
 
@@ -18,6 +19,9 @@
 #define ICMP_TYPE_TIMESTAMPREPLY 14
 #define ICMP_TYPE_INFO_REQUEST 15
 #define ICMP_TYPE_INFO_REPLY 16
+
+extern int
+icmp_output(uint8_t type, uint8_t code, uint32_t values, const uint8_t *data, size_t len, ip_addr_t src, ip_addr_t dst);
 
 extern int icmp_init();
 
